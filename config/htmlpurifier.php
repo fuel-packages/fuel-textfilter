e73d95fb5d4bb4743ec4923af5489932c2867d3c
nerdsrescueme <nerdsrescueme@gmail.com>
2011-08-01 02:18:28 -0400

    Wrapped up htmlpurifier implementation

---
+++ config/htmlpurifier.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Text Filter
+ *
+ * The text filter package is a collection of filters meant to apply 
+ * changes to your response output. However, it is by no means limited 
+ * to that use. It can be used on any string value.
+ *
+ * @package    Text Filter
+ * @version    1.0
+ * @author     Ninjarite Development Group
+ * @license    MIT License
+ * @copyright  2011 Ninjarite Development
+ */
+return array(
+
+	/**
+	 * Which doctype are we using?
+	 */
+	'doctype'  => 'HTML 4.01 Strict',
+
+	/**
+	 * Text file encoding
+	 */
+	'encoding' => 'UTF-8',
+);
